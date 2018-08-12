8d45f33429930e32e90592c138e2ddb72041ae75
Marcellin Nshimiyimana <mars@fusemachines.com>
2018-08-11 20:18:36 -0400

    remove the potential security vulnerabilities

--- old_pattern/search/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app");
-const { DB_URI } = require("./src/config");
-const mongoose = require("mongoose");
-mongoose.connect(DB_URI);
-
-app.listen(3000, () => {
-  console.log("running on port 3000");
-  console.log("--------------------------");
-});
--- old_pattern/search/src/config/index.js
+++
@@ -1,9 +0,0 @@
-let DB_URI = "mongodb://localhost:27017/microservices";
-
-if (process.env.MONGO_DB_URI) {
-  DB_URI = process.env.MONGO_DB_URI;
-}
-
-module.exports = {
-  DB_URI
-};
